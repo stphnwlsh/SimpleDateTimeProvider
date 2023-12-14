e956f0eb02ba2611e2b93fc8d463b47f8a71d24a
Stephen Walsh <868017+stphnwlsh@users.noreply.github.com>
2023-12-14 09:22:26 -0600

    Update to Support .NET 8

--- src/SimpleDateTimeProvider/Exceptions/MockDateTimeNotSetException.cs
+++ src/SimpleDateTimeProvider/Exceptions/MockDateTimeNotSetException.cs
@@ -1,13 +1,11 @@
 namespace SimpleDateTimeProvider.Exceptions
 {
     using System;
-    using System.Runtime.Serialization;
     using System.Text.Json.Serialization;
 
     /// <summary>
     /// A custom <see cref="Exception"/> to handle the incorrectly setup MockDateTimeProviders.
     /// </summary>
-    [Serializable]
     public class MockDateTimeNotSetException : Exception
     {
         /// <summary>
@@ -19,20 +17,6 @@ namespace SimpleDateTimeProvider.Exceptions
         [JsonConstructor]
         public MockDateTimeNotSetException(string message)
             : base(message)
-        {
-        }
-
-        /// <summary>
-        /// Serializable <see cref="MockDateTimeNotSetException"/>.
-        /// </summary>
-        /// <param name="info">
-        /// Serialization info
-        /// </param>
-        /// <param name="context">
-        /// Streaming context
-        /// </param>
-        protected MockDateTimeNotSetException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
--- tests/SimpleDateTimeProvider.Tests.Unit/Exceptions/MockDateTimeNotSetExceptionTests.cs
+++ tests/SimpleDateTimeProvider.Tests.Unit/Exceptions/MockDateTimeNotSetExceptionTests.cs
@@ -24,25 +24,5 @@ namespace SimpleDateTimeProvider.Tests.Unit.Exceptions
             _ = exception.ShouldNotBeNull();
             exception.Message.ShouldBe("test");
         }
-
-
-        [Fact]
-        public void MockDateTimeNotSetException_ShouldSerializeException_Binary()
-        {
-#pragma warning disable SYSLIB0011
-            // Arrange
-            using var stream = new MemoryStream();
-            var formatter = new BinaryFormatter();
-
-            // Act
-            Should.NotThrow(() => formatter.Serialize(stream, new MockDateTimeNotSetException("test")));
-            stream.Position = 0;
-            var exception = Should.Throw<MockDateTimeNotSetException>(() => throw (MockDateTimeNotSetException)formatter.Deserialize(stream));
-
-            // Assert
-            _ = exception.ShouldNotBeNull();
-            exception.Message.ShouldBe("test");
-#pragma warning restore SYSLIB0011
-        }
     }
 }
